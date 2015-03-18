1c375352c878490744ff96fe241635f54803b63e
Thiago Costa <thg.humble@gmail.com>
2015-03-18 00:01:38 -0300

    meu segundo commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+$("#nav").navgoco({accordion: true, save: false})
+})
+
+
+$(function(){
+	$('nav#navigation ul li a').hover(function(){ /*quando passar o mouse faz:*/
+		var newWidth = $(this).width() + 16;      /* acao ao passar o mouse*/
+		var newLeft  = $(this).position().left;
+		
+		$('span.linha').stop().animate({
+			width: newWidth,
+			left: newLeft
+		},300);
+	});
+});
+
+$(document).ready(function() {
+    $("#demo2").navgoco({accordion: true});
+})
